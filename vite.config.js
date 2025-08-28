3438bb1e492aa5af45069b2312202ab076f12e55
philbarton <GRwWYPae9dCpzL>
2025-08-28 14:55:21 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     server: {
-        allowedHosts: [
-            "localhost",
-            "*.netlify.app" // 👈 add your Netlify preview host
-        ]
+        allowedHosts: true
     },
 });
