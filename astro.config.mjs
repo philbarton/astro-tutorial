927fb70122662c5f5ce9a655df9caed008b91016
philbarton <GRwWYPae9dCpzL>
2025-08-28 10:36:19 +0100

    netlify server defer

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
-import node from '@astrojs/node';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [preact()],
 
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: netlify(),
 });
